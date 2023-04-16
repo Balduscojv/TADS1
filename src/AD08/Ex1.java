c926b5a56945b613a476dfcddba51f0badab88dc
Balduscojv <joao.vbsilva30@senacsp.edu.br>
2023-04-16 13:09:04 -0300

    atualiza

--- src/AD08/Ex1.java
+++ src/AD08/Ex1.java
@@ -4,28 +4,35 @@ package AD08;
 import java.util.Random;
 
 public class Ex1 {
+
+
     public static void main(String[] args) {
-        {
-            Random aleatorio = new Random();
-            int repeticao = 0;
-            int[] contagemFaces = new int[6];
-            int numLancamentos = 1000000;
 
-            do {
-                int dado = aleatorio.nextInt(6) + 1;
-                System.out.println("Dado lançado..... numero exibido: " + dado);
+        Random aleatorio = new Random();
+        int[] contagemFaces = new int[6];
+        int numLancamentos = 1000000;
+        int num1, num2, num3, num4, num5, num6, repeticao = 0;
 
-                contagemFaces[dado - 1]++;
-                repeticao++;
-            } while (repeticao <= numLancamentos);
+        do {
+            int dado = aleatorio.nextInt(6) + 1;
+            System.out.println("Dado lançado..... numero exibido: " + dado);
 
-            for (int i = 0; i < 6; i++) {
-                double porcentagem = (double) contagemFaces[i] / numLancamentos * 100;
-                System.out.printf("Face %d: %.2f%%\n", i + 1, porcentagem);
-            }
+            contagemFaces[dado - 1]++;
+            repeticao++;
+
+        } while (repeticao <= numLancamentos);
+
+        for (int i = 0; i < 6; i++) {
+            double porcentagem = (double) contagemFaces[i] / numLancamentos * 100;
+            System.out.printf("Face %d: %.2f%%\n", i + 1, porcentagem);
+            //System.out.printf("numero %d: %d\n ", i, contagemFaces[i]);
         }
     }
 }
+
+
+
+
 
 
 
