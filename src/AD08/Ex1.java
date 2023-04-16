e96e6dbdceede39ff98d1d3fc2f3b3577d705a90
Balduscojv <joao.vbsilva30@senacsp.edu.br>
2023-04-16 12:03:46 -0300

    atualizar

--- src/AD08/Ex1.java
+++ src/AD08/Ex1.java
@@ -5,20 +5,30 @@ import java.util.Random;
 
 public class Ex1 {
     public static void main(String[] args) {
-        Random aleatorio = new Random();
-        int repeticao = 0;
+        {
+            Random aleatorio = new Random();
+            int repeticao = 0;
+            int[] contagemFaces = new int[6];
+            int numLancamentos = 1000000;
 
-        do {
-            int dado = (int) (Math.random() * 6 + 1);
-            System.out.println("Dado lançado..... numero exibido: " + dado);
-            repeticao++;
-        }while(repeticao <= 1000000);
+            do {
+                int dado = aleatorio.nextInt(6) + 1;
+                System.out.println("Dado lançado..... numero exibido: " + dado);
 
+                contagemFaces[dado - 1]++;
+                repeticao++;
+            } while (repeticao <= numLancamentos);
 
-
+            for (int i = 0; i < 6; i++) {
+                double porcentagem = (double) contagemFaces[i] / numLancamentos * 100;
+                System.out.printf("Face %d: %.2f%%\n", i + 1, porcentagem);
+            }
         }
-
-
-
     }
+}
+
+
+
+
+
 
