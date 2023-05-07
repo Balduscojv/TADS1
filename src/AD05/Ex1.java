d51d203368823229d986f9b7ea87f2d12e50157b
Balduscojv <joao.vbsilva30@senacsp.edu.br>
2023-05-07 20:04:33 -0300

    novo comit

--- src/AD05/Ex1.java
+++ src/AD05/Ex1.java
@@ -39,6 +39,8 @@ public class Ex1 {
 
             }
         } while(choice != 4);
+
+
     }
 
 }
--- src/AD09/Equacao.java
+++ src/AD09/Equacao.java
@@ -1,2 +1,25 @@
-package AD09;public class Equacao {
+package AD09;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Equacao {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Dê um valor minimo: ");
+        int min = input.nextInt();
+
+        System.out.print("agora um valor maximo: ");
+        int max = input.nextInt();
+
+        Random random = new Random();
+        int a = random.nextInt((max - min) + 1) + min;
+        int b = random.nextInt((max - min) + 1) + min;
+
+        System.out.printf("A equação é  %dx + %d = 0\n", a, b);
+
+        double x = -b / (double) a;
+        System.out.printf("Resolução:  x = %.2f\n", x);
+    }
 }
